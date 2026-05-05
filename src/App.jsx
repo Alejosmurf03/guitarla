fd0bc5375bd26bbe25cd2363f3ab644538feef13
AlejosmurfLegal <manuel.vega@legalsuitelatam.com>
2026-05-05 17:00:41 -0500

    Feat: Se realiza envio de props para el carrito de compras y modificar los datos del componente guitar, funcion para agregar items al carrito

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,31 @@ import { db } from "./data/db";
 
 function App() {
   const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
+
+  function addToCart(item) {
+    const itemExists = cart.findIndex((cartItem) => cartItem.id === item.id);
+    if (itemExists >= 0) {
+      const updatedCart = [...cart];
+      updatedCart[itemExists].quantity += 1;
+      setCart(updatedCart);
+    } else {
+      item.quantity = 1;
+      setCart([...cart, item]);
+    }
+  }
 
   return (
     <>
-      <Header />
+      <Header 
+        cart={cart}
+      />
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
           {data.map((guitar) => (
-            <Guitar key={guitar.id} />
+            <Guitar key={guitar.id} guitar={guitar} addToCart={addToCart} />
           ))}
         </div>
       </main>
