653efa67ba0bfda46c5c6a6ba3292b9344d21172
AlejosmurfLegal <manuel.vega@legalsuitelatam.com>
2026-05-07 10:16:37 -0500

    Feat: se guarda el state del carrito en el local storage para persistencia de datos

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Guitar from "./components/Guitar";
 import { db } from "./data/db";
 
 function App() {
-  const [data, setData] = useState(db);
-  const [cart, setCart] = useState([]);
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem("cart");
+    return localStorageCart ? JSON.parse(localStorageCart) : [];
+  }
+
+  const [data] = useState(db);
+  const [cart, setCart] = useState(initialCart);
 
   const MAX_QUANTITY = 5;
   const MIN_QUANTITY = 1;
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
 
   function addToCart(item) {
     const itemExists = cart.findIndex((cartItem) => cartItem.id === item.id);
